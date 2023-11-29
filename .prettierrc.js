21908ddd1f0e09717aaae2e11d4245e4a60af12a
TinajeroOC <88462409+TinajeroOC@users.noreply.github.com>
2023-11-28 21:37:11 -0800

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  plugins: ['prettier-plugin-tailwindcss']
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main className="mx-auto flex h-auto w-full max-w-7xl justify-center">Nook</main>
+}
