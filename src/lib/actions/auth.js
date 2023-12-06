3b2583c2f1dbc24c971524bda3d92b00094a9907
TinajeroOC <88462409+TinajeroOC@users.noreply.github.com>
2023-12-06 10:28:36 -0800

    Update PocketBase collections configuration

--- src/lib/actions/auth.js
+++ src/lib/actions/auth.js
@@ -12,34 +12,28 @@ export async function createUser({ username, name, email, password }) {
 
   const pb = await initPocketBaseServer(true)
 
-  /**
-   * Default settings record must use some unique use information (e.g username),
-   * else PocketBase will try to use an existing record with the same field values.
-   */
-  const settings = await pb.collection('settings').create({
-    theme: 'blue',
-    bio: `${name} (@${username}), Nook Newcomer`,
-    status: 'Lounging',
-    isNameVisible: true,
-    useGradientBg: true,
-  })
-
-  return await pb.collection('users').create({
+  const user = await pb.collection('users').create({
     username: username.toLowerCase(),
     name,
     email,
     password,
     passwordConfirm: password,
-    settings: settings.id,
+  })
+
+  await pb.collection('settings').create({
+    user: user.id,
+    theme: 'blue',
+    bio: `Nook Newcomer`,
+    status: 'Lounging',
+    isNameVisible: true,
+    useGradientBg: true,
   })
 }
 
 export async function authenticateUser({ email, password }) {
   const pb = await initPocketBaseServer(true)
 
-  const result = await pb.collection('users').authWithPassword(email, password)
+  await pb.collection('users').authWithPassword(email, password)
 
   cookies().set('pb_auth', pb.authStore.exportToCookie())
-
-  return result
 }
