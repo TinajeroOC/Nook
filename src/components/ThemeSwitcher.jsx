7988e4d0b72c89cc92cbed363730b21301f049b2
TinajeroOC <88462409+TinajeroOC@users.noreply.github.com>
2023-12-08 19:16:19 -0800

    Add custom style to theme switcher component

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -1,9 +1,37 @@
 'use client'
 
-import { Switch } from '@nextui-org/react'
+import { useSwitch, VisuallyHidden } from '@nextui-org/react'
 import { IconMoonFilled, IconSunFilled } from '@tabler/icons-react'
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
+
+const StyledSwitch = (props) => {
+  const { Component, slots, isSelected, getBaseProps, getInputProps, getWrapperProps } =
+    useSwitch(props)
+
+  return (
+    <>
+      <Component {...getBaseProps()}>
+        <VisuallyHidden>
+          <input {...getInputProps()} />
+        </VisuallyHidden>
+        <div
+          {...getWrapperProps()}
+          className={slots.wrapper({
+            class: [
+              'h-10 w-10',
+              'flex items-center justify-center',
+              'rounded-xl bg-default-50 hover:bg-default-100',
+              'mr-0',
+            ],
+          })}
+        >
+          {isSelected ? <IconSunFilled size='20' /> : <IconMoonFilled size='20' />}
+        </div>
+      </Component>
+    </>
+  )
+}
 
 export default function ThemeSwitcher() {
   const [mounted, setMounted] = useState(false)
@@ -16,12 +44,9 @@ export default function ThemeSwitcher() {
   if (!mounted) return null
 
   return (
-    <Switch
-      isSelected={theme === 'dark' ? true : false}
-      color='primary'
-      startContent={<IconSunFilled />}
-      endContent={<IconMoonFilled />}
-      onValueChange={(isSelected) => (isSelected ? setTheme('dark') : setTheme('light'))}
-    ></Switch>
+    <StyledSwitch
+      isSelected={theme === 'dark' ? false : true}
+      onValueChange={(isSelected) => (isSelected ? setTheme('light') : setTheme('dark'))}
+    />
   )
 }
