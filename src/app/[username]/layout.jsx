eb851cdcbdf867970a7260e3af602b2ac4f944a4
TinajeroOC <88462409+TinajeroOC@users.noreply.github.com>
2023-12-08 18:50:56 -0800

    Update user page layouts
    Match padding with base layout.

--- src/app/[username]/layout.jsx
+++ src/app/[username]/layout.jsx
@@ -3,7 +3,7 @@ import UserFooter from '@/components/layouts/Footer/UserFooter'
 export default function NookLayout({ children }) {
   return (
     <div className='relative flex flex-col'>
-      <main className='container mx-auto min-h-[calc(100vh_-_93px)] max-w-xl flex-grow p-8'>
+      <main className='container mx-auto min-h-[calc(100vh_-_73px)] max-w-xl flex-grow p-6'>
         {children}
       </main>
       <UserFooter />
--- src/components/layouts/Footer/UserFooter.jsx
+++ src/components/layouts/Footer/UserFooter.jsx
@@ -2,10 +2,10 @@ import { Divider, Link } from '@nextui-org/react'
 
 export default function UserFooter() {
   return (
-    <footer className='container mx-auto max-w-xl px-8 pb-8'>
-      <div className='flex flex-col items-center justify-center gap-8'>
+    <footer className='container mx-auto max-w-xl px-6 pb-6'>
+      <div className='flex flex-col items-center justify-center gap-5'>
         <Divider />
-        <Link href='/' color='foreground' className='text-xl font-light text-default-500'>
+        <Link href='/' color='foreground' className='font-light text-default-500'>
           Nook
         </Link>
       </div>
