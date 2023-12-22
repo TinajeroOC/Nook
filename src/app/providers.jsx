2a1e91f94765dd7b99ed204655da4a12784a7598
TinajeroOC <88462409+TinajeroOC@users.noreply.github.com>
2023-12-22 11:08:34 -0800

    Use client side routing for NextUI components

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,11 +1,14 @@
 'use client'
 
 import { NextUIProvider } from '@nextui-org/react'
+import { useRouter } from 'next/navigation'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
 export default function Providers({ children }) {
+  const router = useRouter()
+
   return (
-    <NextUIProvider>
+    <NextUIProvider navigate={router.push}>
       <NextThemesProvider defaultTheme='dark'>{children}</NextThemesProvider>
     </NextUIProvider>
   )
