0440d4f2d38985a0282f6cd0ab336ac0c0acbdea
TinajeroOC <88462409+TinajeroOC@users.noreply.github.com>
2023-12-27 14:43:29 -0800

    Update data actions
    Use try-catch and log errors

--- src/actions/data.js
+++ src/actions/data.js
@@ -2,20 +2,32 @@
 
 import { initPocketBaseServer } from '../lib/pocketbase'
 
-export async function createCollectionRecord({ collectionName, data }) {
-  const pb = await initPocketBaseServer()
-
-  return await pb.collection(collectionName).create(data)
+export async function createCollectionRecord(collectionName, data) {
+  try {
+    const pb = await initPocketBaseServer()
+    return await pb.collection(collectionName).create(data)
+  } catch (error) {
+    console.log(`There was an issue creating a record in collection ${collectionName}: ${error}`)
+    throw error
+  }
 }
 
-export async function updateCollectionRecord({ collectionName, recordId, data }) {
-  const pb = await initPocketBaseServer()
-
-  return await pb.collection(collectionName).update(recordId, data)
+export async function updateCollectionRecord(collectionName, recordId, data) {
+  try {
+    const pb = await initPocketBaseServer()
+    return await pb.collection(collectionName).update(recordId, data)
+  } catch (error) {
+    console.log(`There was an issue updating a record in collection ${collectionName}: ${error}`)
+    throw error
+  }
 }
 
-export async function deleteCollectionRecord({ collectionName, recordId }) {
-  const pb = await initPocketBaseServer()
-
-  return await pb.collection(collectionName).delete(recordId)
+export async function deleteCollectionRecord(collectionName, recordId) {
+  try {
+    const pb = await initPocketBaseServer()
+    return await pb.collection(collectionName).delete(recordId)
+  } catch (error) {
+    console.log(`There was an issue deleting a record in collection ${collectionName}: ${error}`)
+    throw error
+  }
 }
